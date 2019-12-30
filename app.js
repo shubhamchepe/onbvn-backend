f524d750727231ea54bb070d13a4e6ce1e95cc8e
shubhamchepe <hemantchepe@gmail.com>
2019-12-30 19:57:59 +0530

    V Commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||3000;
 const {getAllUsers,
        createUser,
        getUserById,
