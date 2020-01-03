9ebfd1c5e713f80c279ee834b7a4b9726e9d0eb4
shubhamchepe <hemantchepe@gmail.com>
2020-01-03 18:39:08 +0530

    VIII Commit

--- controllers/user.js
+++ controllers/user.js
@@ -5,30 +5,30 @@ const config = require('../config');
 const AWS = require('aws-sdk');
 const fs = require('fs');
 
-// Enter copied or downloaded access ID and secret key here
-const ID = 'AKIAJLIIFPP4RSF4MICA';
-const SECRET = '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';
+// // Enter copied or downloaded access ID and secret key here
+// const ID = 'AKIAJLIIFPP4RSF4MICA';
+// const SECRET = '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';
 
-// The name of the bucket that you have created
-const BUCKET_NAME = 'test-bucket';
+// // The name of the bucket that you have created
+// const BUCKET_NAME = 'test-bucket';
 
-const s3 = new AWS.S3({
-    accessKeyId: ID,
-    secretAccessKey: SECRET
-});
+// const s3 = new AWS.S3({
+//     accessKeyId: ID,
+//     secretAccessKey: SECRET
+// });
 
-const params = {
-    Bucket: BUCKET_NAME,
-    CreateBucketConfiguration: {
-        // Set your region here
-        LocationConstraint: "ap-south-1"
-    }
-};
+// const params = {
+//     Bucket: BUCKET_NAME,
+//     CreateBucketConfiguration: {
+//         // Set your region here
+//         LocationConstraint: "ap-south-1"
+//     }
+// };
 
-s3.createBucket(params, function(err, data) {
-    if (err) console.log(err, err.stack);
-    else console.log('Bucket Created Successfully', data.Location);
-});
+// s3.createBucket(params, function(err, data) {
+//     if (err) console.log(err, err.stack);
+//     else console.log('Bucket Created Successfully', data.Location);
+// });
 
 // const uploadFile = (fileName) => {
 //     // Read content from the file
@@ -64,20 +64,20 @@ const createUser = (req,res) => {
     newUser.aadharFrontImage =   req.body.aadharFrontImage;
     newUser.aadharBackImage =   req.body.aadharBackImage;
 
-    const params = {
-        Bucket: BUCKET_NAME,
-        Key: 'cat.jpeg', // File name you want to save as in S3
-        Body: fileContent,
-        ContentType: "image/jpeg"
-    };
+    // const params = {
+    //     Bucket: BUCKET_NAME,
+    //     Key: 'cat.jpeg', // File name you want to save as in S3
+    //     Body: fileContent,
+    //     ContentType: "image/jpeg"
+    // };
 
-    // Uploading files to the bucket
-    s3.upload(params, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
-    });
+    // // Uploading files to the bucket
+    // s3.upload(params, function(err, data) {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     console.log(`File uploaded successfully. ${data.Location}`);
+    // });
 
     newUser.save((err,newUser)=>{
        if(err){
