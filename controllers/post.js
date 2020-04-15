68522c5dc882fa63d8ff0add3c82d8750d3def82
shubhamchepe <hemantchepe@gmail.com>
2020-04-15 20:47:39 +0530

    105

--- controllers/post.js
+++ controllers/post.js
@@ -77,7 +77,7 @@ const GetAllPosts = async (req,res) => {
                     }else{
                         res.json(data)
                     }
-                })
+                }).catch(err => console.log(err))
             }
         });
 
