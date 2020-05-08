babeedb15cb0a12a76738fa284ed3c4ce818324d
shubhamchepe <hemantchepe@gmail.com>
2020-05-08 20:02:25 +0530

    214

--- app.js
+++ app.js
@@ -55,19 +55,7 @@ connectDb();
      app.use(cors());
 // mongoose.connect(db, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true,useUnifiedTopology: true });
 //app.use((req, res, next) => {
-       res.header(`Access-Control-Allow-Origin`, `*`);
-       res.header(
-           `Access-Control-Allow-Headers`,
-           `Origin, X-Requested-With, Content-Type, Accept, Authorization`
-       );
-   
-       if (req.method === `OPTIONS`) {
-           res.header(`Access-Control-Allow-Methods`, `PUT, POST, PATCH, DELETE, GET`);
-           return res.status(200).json({});
-       }
-   
-       next();
-   });
+      
 // console.log('DB Connected!'); 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
