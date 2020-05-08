99e19db145edc1623c2722de0a15d48abebe8dc4
shubhamchepe <hemantchepe@gmail.com>
2020-05-08 20:00:47 +0530

    213

--- app.js
+++ app.js
@@ -48,12 +48,26 @@ const connectDb = require('./utils/connectDb');
 
 connectDb();
 // cors origin URL - Allow inbound traffic from origin
-corsOptions = {
-       origin: "https://onbvn-backend.herokuapp.com",
-       optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-     };
-     app.use(cors(corsOptions));
+// corsOptions = {
+//        origin: "https://onbvn-backend.herokuapp.com",
+//        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//      };
+     app.use(cors());
 // mongoose.connect(db, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true,useUnifiedTopology: true });
+//app.use((req, res, next) => {
+       res.header(`Access-Control-Allow-Origin`, `*`);
+       res.header(
+           `Access-Control-Allow-Headers`,
+           `Origin, X-Requested-With, Content-Type, Accept, Authorization`
+       );
+   
+       if (req.method === `OPTIONS`) {
+           res.header(`Access-Control-Allow-Methods`, `PUT, POST, PATCH, DELETE, GET`);
+           return res.status(200).json({});
+       }
+   
+       next();
+   });
 // console.log('DB Connected!'); 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -125,7 +139,7 @@ app.get('/GetId', verifyToken,CheckIfFriends);
 //Get Pending Users
 app.get('/getPendingusers',getPendingUsers);
 //Accept New User
-app.post('/AcceptNewUser', AcceptNewUser);
+app.post('/AcceptNewUser/:id', AcceptNewUser);
 
 
 
